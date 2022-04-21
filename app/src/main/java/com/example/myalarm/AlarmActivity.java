0018f7478f7adb69bd8fea7798665a3121ddcbe1
Sanghyun Yi <njsharry32@gmail.com>
2022-04-21 16:06:47 +0900

    init ui page and change launcher page

---
+++ app/src/main/java/com/example/myalarm/AlarmActivity.java
@@ -0,0 +1,36 @@
+package com.example.myalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class AlarmActivity extends AppCompatActivity {
+
+
+    private TextView txView_notAlarm;
+    private LinearLayout lout_add_option;
+    private Button btn_add;
+    private Button btn_option;
+    private ListView liView_alarm;
+    private Button btn_stopWatch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+
+        //init variation
+
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/myalarm/LogInActivity.java
+++ app/src/main/java/com/example/myalarm/LogInActivity.java
@@ -2,13 +2,27 @@ package com.example.myalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private Button btn_signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        btn_signIn = (Button) findViewById(R.id.btn_signIn);
+        btn_signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogInActivity.this, AlarmActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
