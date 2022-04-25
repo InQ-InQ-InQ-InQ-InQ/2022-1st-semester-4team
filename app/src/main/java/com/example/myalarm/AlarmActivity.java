f153e1af5711997db2e22984456bee58215b3cb3
Sanghyun Yi <njsharry32@gmail.com>
2022-04-25 16:04:30 +0900

    connect alarm page to other page

--- app/src/main/java/com/example/myalarm/AlarmActivity.java
+++ app/src/main/java/com/example/myalarm/AlarmActivity.java
@@ -19,6 +19,7 @@ public class AlarmActivity extends AppCompatActivity {
     private Button btn_option;
     private ListView liView_alarm;
     private Button btn_stopWatch;
+    private Button btn_timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +27,24 @@ public class AlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alarm);
 
         //init variation
+        btn_stopWatch = (Button) findViewById(R.id.btn_stopWatch);
+        btn_timer = (Button) findViewById(R.id.btn_timer);
 
+        btn_stopWatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AlarmActivity.this, StopWatchActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AlarmActivity.this, TimerActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
