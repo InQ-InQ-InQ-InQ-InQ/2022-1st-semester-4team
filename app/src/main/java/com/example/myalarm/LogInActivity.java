a81878cb13d223f6707de8d64ddabed1203a3f9d
Sanghyun Yi <njsharry32@gmail.com>
2022-05-19 15:56:19 +0900

    finish join member UI, have to finish all page UI first

--- app/src/main/java/com/example/myalarm/LogInActivity.java
+++ app/src/main/java/com/example/myalarm/LogInActivity.java
@@ -14,6 +14,10 @@ public class LogInActivity extends AppCompatActivity {
     private EditText et_id;
     private EditText et_password;
     private Button btn_signIn;
+    private Button btn_signUp;
+    private Button btn_rememberId;
+    private Button btn_forgetId;
+    private Button btn_forgetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,10 @@ public class LogInActivity extends AppCompatActivity {
         et_id = (EditText) findViewById(R.id.et_id);
         et_password = (EditText) findViewById(R.id.et_password);
         btn_signIn = (Button) findViewById(R.id.btn_signIn);
-
+        btn_signUp = (Button) findViewById(R.id.btn_signUp);
+        btn_rememberId = (Button) findViewById(R.id.btn_rememberId);
+        btn_forgetId = (Button) findViewById(R.id.btn_forgetId);
+        btn_forgetPassword = (Button) findViewById(R.id.btn_forgetPassword);
 
         btn_signIn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +42,16 @@ public class LogInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btn_signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LogInActivity.this, JoinActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
 
 
