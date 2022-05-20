7c73b9c6b141a85e9806abe316e0c0ff2e9e2009
Sanghyun Yi <njsharry32@gmail.com>
2022-05-20 16:05:51 +0900

    have to fix stopwatch function

--- app/src/main/java/com/example/myalarm/StopWatchActivity.java
+++ app/src/main/java/com/example/myalarm/StopWatchActivity.java
@@ -2,15 +2,24 @@ package com.example.myalarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class StopWatchActivity extends AppCompatActivity {
 
+    private TextView tv_stopwatch_viewer;
     private Button btn_alarm;
     private Button btn_timer;
+    private Button btn_stopwatch_start;
+    private Thread timeThread = null;
+    private Boolean isRunning = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,8 @@ public class StopWatchActivity extends AppCompatActivity {
 
         btn_alarm = (Button) findViewById(R.id.btn_alarm);
         btn_timer = (Button) findViewById(R.id.btn_timer);
+        btn_stopwatch_start = (Button) findViewById(R.id.btn_stopWatch_start);
+        tv_stopwatch_viewer = (TextView) findViewById(R.id.tv_stopwatch_viewer);
 
         btn_alarm.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +46,63 @@ public class StopWatchActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btn_stopwatch_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setVisibility(View.GONE);
+                Toast.makeText(StopWatchActivity.this, "1분 15초가 지났습니다.", Toast.LENGTH_SHORT).show();
+
+                timeThread = new Thread(new timeThread());
+                timeThread.start();
+            }
+        });
+    }
+
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int mSec = msg.arg1 % 100;
+            int sec = (msg.arg1 / 100) % 60;
+            int min = (msg.arg1 / 100) / 60;
+            int hour = (msg.arg1 / 100) / 360;
+            //1000이 1초 1000*60 은 1분 1000*60*10은 10분 1000*60*60은 한시간
+
+            @SuppressLint("DefaultLocale") String result = String.format("%02d:%02d:%02d:%02d", hour, min, sec, mSec);
+            if (result.equals("00:01:15:00")) {
+                Toast.makeText(StopWatchActivity.this, "1분 15초가 지났습니다.", Toast.LENGTH_SHORT).show();
+            }
+            //mTimeTextView.setText(result);
+        }
+    };
+
+    public class timeThread implements Runnable {
+        @Override
+        public void run() {
+            int i = 0;
+
+            while (true) {
+                while (isRunning) { //일시정지를 누르면 멈춤
+                    Message msg = new Message();
+                    msg.arg1 = i++;
+                    handler.sendMessage(msg);
+
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable(){
+                            @Override
+                            public void run() {
+                                tv_stopwatch_viewer.setText("");
+                                tv_stopwatch_viewer.setText("00:00:00:00");
+                            }
+                        });
+                        return; // 인터럽트 받을 경우 return
+                    }
+                }
+            }
+        }
     }
 }
