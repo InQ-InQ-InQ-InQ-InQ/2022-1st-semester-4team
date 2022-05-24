c2ddd9776bf133f6e74fbc680a35cd7c06104f0e
Sanghyun Yi <njsharry32@gmail.com>
2022-05-24 14:45:53 +0900

    implement most of stopwatch function, have to remove some implements and elaborate UI

--- app/src/main/java/com/example/myalarm/StopWatchActivity.java
+++ app/src/main/java/com/example/myalarm/StopWatchActivity.java
@@ -3,6 +3,8 @@ package com.example.myalarm;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -15,9 +17,10 @@ import android.widget.Toast;
 
 public class StopWatchActivity extends AppCompatActivity {
 
-    private TextView tv_stopwatch_viewer;
+    private TextView tv_stopwatch_viewer, tv_record_viewer;
     private Button btn_alarm, btn_timer;
-    private Button btn_stopwatch_start, btn_stopwatch_record;
+    private Button btn_start_pause_resume, btn_record_init;
+
 
     //status
     public static final int INIT = 0;
@@ -37,9 +40,13 @@ public class StopWatchActivity extends AppCompatActivity {
         tv_stopwatch_viewer = (TextView) findViewById(R.id.tv_stopwatch_viewer);
         btn_alarm = (Button) findViewById(R.id.btn_alarm);
         btn_timer = (Button) findViewById(R.id.btn_timer);
-        btn_stopwatch_start = (Button) findViewById(R.id.btn_stopWatch_start);
-        btn_stopwatch_record = (Button) findViewById(R.id.btn_stopWatch_record);
+        btn_start_pause_resume = (Button) findViewById(R.id.btn_start_pause_resume);
+        btn_record_init = (Button) findViewById(R.id.btn_record_init);
+        tv_record_viewer = (TextView) findViewById(R.id.tv_record_viewer);
+        tv_record_viewer.setVisibility(View.GONE);
 
+
+        //clickLister
         btn_alarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,56 +63,75 @@ public class StopWatchActivity extends AppCompatActivity {
             }
         });
 
-        btn_stopwatch_start.setOnClickListener(new View.OnClickListener() {
+        btn_start_pause_resume.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                view.setVisibility(View.GONE);
-                Toast.makeText(StopWatchActivity.this, "스톱워치를 시작합니다", Toast.LENGTH_SHORT).show();
-                staButton();
+                btn_start_pause_resume.setText("PAUSE");
+                btn_start_pause_resume.setBackgroundResource(R.drawable.btn_red);
+                //Toast.makeText(StopWatchActivity.this, "스톱워치를 시작합니다", Toast.LENGTH_SHORT).show();
+                start_pause_resume();
+            }
+        });
+
+        btn_record_init.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                record_init();
             }
         });
 
     }
 
-    private void staButton(){
+    private void start_pause_resume() {
         switch (status){
             case INIT:
-                //어플리케이션이 실행되고 나서 실제로 경과된 시간...
                 baseTime = SystemClock.elapsedRealtime();
-                System.out.println("1234");
-                //핸들러 실행
                 handler.sendEmptyMessage(0);
-                btn_stopwatch_start.setText("멈춤");
-                //btn_stopwatch_record.setEnabled(true);
-
-                //상태 변환
+                btn_start_pause_resume.setText("PAUSE");
                 status = RUN;
                 break;
             case RUN:
-                //핸들러 정지
                 handler.removeMessages(0);
-
-                //정지 시간 체크
                 pauseTime = SystemClock.elapsedRealtime();
-
-                btn_stopwatch_start.setText("시작");
-                btn_stopwatch_record.setText("리셋");
-
-                //상태변환
+                btn_start_pause_resume.setText("RESUME");
+                btn_record_init.setText("RESET");
                 status = PAUSE;
                 break;
             case PAUSE:
                 long reStart = SystemClock.elapsedRealtime();
                 baseTime += (reStart - pauseTime);
-
                 handler.sendEmptyMessage(0);
-
-                btn_stopwatch_start.setText("멈춤");
-                btn_stopwatch_record.setText("기록");
-
+                btn_start_pause_resume.setText("PAUSE");
+                btn_record_init.setText("RECORD");
                 status = RUN;
         }
 
+    }
+
+    private void record_init() {
+        switch (status) {
+            case RUN:
+                tv_record_viewer.setVisibility(View.VISIBLE);
+                String timeList = tv_record_viewer.getText().toString();
+                timeList+= String.format("%2d. %s\n",cnt,getTime());
+                tv_record_viewer.setText(timeList);
+                cnt++;
+                break;
+            case PAUSE:
+                Toast.makeText(StopWatchActivity.this, "RESET", Toast.LENGTH_SHORT).show();
+                tv_stopwatch_viewer.setText("00:00:00");
+                //record.setText("");
+                baseTime = 0;
+                pauseTime = 0;
+                cnt = 1;
+                status = INIT;
+                btn_record_init.setText("RECORD");
+                btn_start_pause_resume.setText("START");
+                btn_start_pause_resume.setBackgroundResource(R.drawable.btn_blue);
+                tv_record_viewer.setVisibility(View.GONE);
+                tv_record_viewer.setText("");
+                break;
+        }
     }
 
     @NonNull
@@ -114,12 +140,11 @@ public class StopWatchActivity extends AppCompatActivity {
         long overTime = nowTime - baseTime;
         long m = overTime/1000/60;
         long s = (overTime/1000)%60;
-        long ms = overTime % 1000;
-        String recTime = String.format("%02d:%02d:%03d",m,s,ms);
+        long ms = overTime % 100000 / 10 % 100;
+        String recTime = String.format("%02d:%02d:%02d",m,s,ms);
         return recTime;
     }
     Handler handler = new Handler(){
-
         @Override
         public void handleMessage(@NonNull Message msg) {
             tv_stopwatch_viewer.setText(getTime());
